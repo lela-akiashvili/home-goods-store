fa15811ced979ae195d5b3d15dee9ae315bb906f
lela-akiashvili <l.akiashvili.doit@itstep.ge>
2024-07-09 16:37:34 +0400

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+burger.addEventListener('click', ()=>{
+    burger.classList.toggle('open');
+    menu.classList.toggle('open');
+
+
+})
